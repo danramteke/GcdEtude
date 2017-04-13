3190cb5267d0b6fb7ee8081c7e31327d7fe291c5
Daniel Ramteke <danramteke@gmail.com>
2017-04-12 22:04:11 -0400

    while loop that fires every 3 seconds

--- Sources/GcdEtude/main.swift
+++ Sources/GcdEtude/main.swift
@@ -1,7 +1,29 @@
-import Fetcher
+
+import Dispatch
+
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin
+#endif
+
+#if DEPLOYMENT_RUNTIME_OBJC || os(Linux)
+    import Foundation
+#else
+    import Foundation
+#endif
 
 print("gcd etude")
 
-let f = Fetcher(name: "asdf")
 
-print(f)
+
+//
+//let scheduler = Scheduler()
+//scheduler.run()
+
+
+while true {
+  let oneSecond: UInt32 = 1000000
+  usleep(3 * oneSecond)
+  print("sleeping")
+}
