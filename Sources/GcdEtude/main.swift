1915981b7559d9425cef9e7fb131eb1778e690cf
Daniel Ramteke <danramteke@gmail.com>
2017-04-12 22:18:19 -0400

    cleaned up imports

--- Sources/GcdEtude/main.swift
+++ Sources/GcdEtude/main.swift
@@ -2,17 +2,7 @@
 import Dispatch
 import Fetcher
 
-#if os(Linux)
-    import Glibc
-#else
-    import Darwin
-#endif
 
-#if DEPLOYMENT_RUNTIME_OBJC || os(Linux)
-    import Foundation
-#else
-    import Foundation
-#endif
 
 print("gcd etude")
 
@@ -24,7 +14,7 @@ let fetchers = [
   Fetcher(name: "Moose"),
 ]
 
-let fetcherGroup = DispatchGroup()
+
 let fetcherQueue = DispatchQueue(label: "FetcherQueue", attributes: DispatchQueue.Attributes.concurrent, target: nil)
 
 while true {
